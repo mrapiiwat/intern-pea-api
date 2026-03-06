6c37e11302bb969b21e613812e61f1fb11f454d2
ShaLeeZ2002 <108208048+Shavies@users.noreply.github.com>
2026-03-06 13:41:41 +0700

    IS_FULL status to function

--- backend/src/modules/application/service.ts
+++ backend/src/modules/application/service.ts
@@ -122,7 +122,13 @@ export class ApplicationService {
       .where(inArray(studentProfiles.userId, userIds));
 
     for (const app of pendingApps) {
-      await this.logAppStatusAction(tx, app.id, actionBy, app.status, "IS_FULL");
+      await this.logAppStatusAction(
+        tx,
+        app.id,
+        actionBy,
+        app.status,
+        "IS_FULL"
+      );
     }
 
     await tx.insert(notifications).values(
